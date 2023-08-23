70fb0474bb9d2b4f91a5f61f14f659f45078746a
rising-dragon401 <risingdragon401@gmail.com>
2023-08-23 22:59:45 +0800

    feat: thanks submit page

---
+++ src/components/ThanksSubmit/styles.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const DivThanksSubmitContainer = styled.div`
+  padding: 2rem;
+  min-height: 43vh;
+`;
+
+export const H1Title = styled.h1`
+  margin-bottom: 2rem;
+  color: #fff;
+`;
+
+export const H2Title = styled.h2`
+  margin-bottom: 2rem;
+  color: #fff;
+`;
